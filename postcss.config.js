b481e703a057bc951c09ffffd01884c5ee3082e8
Suen <383366204@qq.com>
2018-06-01 23:13:40 +0800

    babelrc and postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     plugins: {
+        'postcss-import': {},
+        'postcss-url': {},
         'autoprefixer': {
             browsers: ['last 5 version', 'Android >= 4.0'],
             cascade: true,
